0065eddcc34168b0fc1f2cdb29110e252125ac3f
ariharasutha <ariharasuthan685@gmail.com>
2026-05-29 09:08:45 +0530

    Enhance user edit functionality with validation
    Added session handling and improved error messages. Updated form processing to check for empty fields before updating user information.

--- edit.php
+++ edit.php
@@ -1,54 +1,78 @@
-<link rel="stylesheet" href="style.css">
 <?php
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+
+session_start();
+
 include "db.php";
 
-$id=isset($_GET['id']) ? intval($_GET['id']):0;
+$id = isset($_GET['id']) ? intval($_GET['id']) : 0;
 
-$result=$conn->query("SELECT * FROM user WHERE id=$id");
+$result = $conn->query("SELECT * FROM user WHERE id=$id");
 
-$row=$result->fetch_assoc();
+$row = $result->fetch_assoc();
+
 if(!$row){
-
-    echo "error";
+    echo "User Not Found";
+    exit();
 }
 
-?>
-<form method="POST">
-<input type="hidden" name="user_id" value="<?php echo $row['id'];?>"><br>
-<input type="text" name="name" value="<?php echo $row['name'];?>"><br>
-<input type="number" name="age" value="<?php echo $row['age'];?>"><br>
-<input type="number" name="mobileno" value="<?php echo $row['mobileno'];?>"><br>
-<input type="email" name="email" value="<?php echo $row['email'];?>"><br>
-<input type="password" name="password" value="<?php echo $row['password'];?>">
-<button type="submit" name="update">Update</button>
-</form>
-<?php
 if(isset($_POST['update'])){
-$name=$_POST['name'];
-$age=$_POST['age'];
-$mobileno=$_POST['mobileno'];
-$email=$_POST['email'];
-$password=$_POST['password'];
 
-$sql="UPDATE user SET name='$name',age='$age',mobileno='$mobileno',email='$email',password='$password' WHERE id=$id";
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+    $mobileno = $_POST['mobileno'];
+    $email = $_POST['email'];
 
-if($conn->query($sql)){
+    if(
+        empty($name) ||
+        empty($age) ||
+        empty($mobileno) ||
+        empty($email)
+    ){
+        echo "Please fill all fields";
+    }
 
-header("Location:view.php");
-exit();
+    else{
 
-}else{
+        $sql = "UPDATE user SET
+        name='$name',
+        age='$age',
+        mobileno='$mobileno',
+        email='$email'
+        WHERE id=$id";
 
-echo "Error ".$conn->error;
+        if($conn->query($sql)){
+
+            header("Location:view.php");
+            exit();
+
+        }
+
+        else{
+
+            echo "Error : ".$conn->error;
+
+        }
+
+    }
 
 }
 
-}
 ?>
 
+<form method="POST">
 
-</body>
+<input type="text" name="name"
+value="<?php echo $row['name']; ?>"><br><br>
 
-</html>
+<input type="number" name="age"
+value="<?php echo $row['age']; ?>"><br><br>
+
+<input type="number" name="mobileno"
+value="<?php echo $row['mobileno']; ?>"><br><br>
+
+<input type="email" name="email"
+value="<?php echo $row['email']; ?>"><br><br>
+
+<button type="submit" name="update">Update</button>
+
+</form>
