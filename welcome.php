87d386c9304a705082b00dec26c5422cbee30c1a
ariharasutha <ariharasuthan685@gmail.com>
2026-05-29 09:05:54 +0530

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,12 +1,27 @@
+<?php
+session_start();
+
+if(!isset($_SESSION['user'])){
+    header("Location:login.php");
+    exit();
+}
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>welcome</title>
+    <title>Welcome</title>
 </head>
 <body>
-    <h1>welcome</h1>
-    
+
+<h1>
+Welcome
+<?php echo $_SESSION['user']; ?>
+</h1>
+
+<a href="view.php">View Users</a><br><br>
+
+<a href="logout.php">Logout</a>
+
 </body>
 </html>
